8b52c730b8df9d8ac9b68a568ef6903e66b7bee0
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-07-03 21:16:34 +0200

    Fix some shit

--- prwindow.cpp
+++ prwindow.cpp
@@ -2,8 +2,13 @@
 
 static void prWindow_class_init(PRWindowClass *klass);
 static void prWindow_init(PRWindow *window);
+static void prWindow_destroy(GtkObject *object);
+static void prWindow_size_request(GtkWidget *widget, GtkRequisition *requisition);
+static void prWindow_size_allocate(GtkWidget *widget, GtkAllocation *allocation);
+static void prWindow_realize(GtkWidget *widget);
+static gboolean prWindow_expose(GtkWidget *widget, GdkEventExpose *event);
 
-GtkType prWindow_get_type (void)
+GtkType prWindow_get_type(void)
 {
   static GtkType prWindow_type = 0;
 
@@ -25,6 +30,48 @@ GtkType prWindow_get_type (void)
   return prWindow_type;
 }
 
-static void prWindow_init(PRWindow *window) {};
+GtkWidget * prWindow_new(void)
+{
+   return GTK_WIDGET(gtk_type_new(prWindow_get_type()));
+}
+
+static void prWindow_init(PRWindow *window) 
+{
+
+};
 
 static void prWindow_class_init(PRWindowClass *klass) {};
+
+static void prWindow_destroy(GtkObject *object)
+{
+  PRWindow *pr;
+  PRWindowClass *klass;
+
+  g_return_if_fail(object != NULL);
+  g_return_if_fail(IS_PRWINDOW(object));
+
+  pr = PR_WINDOW(object);
+
+  //klass = gtk_type_class(gtk_widget_get_type());
+
+  if (GTK_OBJECT_CLASS(klass)->destroy) {
+     (* GTK_OBJECT_CLASS(klass)->destroy) (object);
+  }
+}
+
+static void gtk_cpu_class_init(PRWindowClass *klass)
+{
+  GtkWidgetClass *widget_class;
+  GtkObjectClass *object_class;
+
+
+  widget_class = (GtkWidgetClass *) klass;
+  object_class = (GtkObjectClass *) klass;
+
+  widget_class->realize = prWindow_realize;
+  widget_class->size_request = prWindow_size_request;
+  widget_class->size_allocate = prWindow_size_allocate;
+  widget_class->expose_event = prWindow_expose;
+
+  object_class->destroy = prWindow_destroy;
+}
--- prwindow.h
+++ prwindow.h
@@ -7,7 +7,7 @@
 extern "C" {
 #endif /* __cplusplus */
 
-#define PR_WINDOW(obj)   GTK_CHECK_CAST (obj, prWindow_get_type (), PrWindow)
+#define PR_WINDOW(obj)   GTK_CHECK_CAST (obj, prWindow_get_type (), PRWindow)
 #define PR_WINDOW_CLASS(klass)  GTK_CHECK_CLASS_CAST (klass, prWindow_get_type (), PRWindowClass)
 #define IS_PRWINDOW(obj)       GTK_CHECK_TYPE (obj, prWindow_get_type ())
 
@@ -27,8 +27,8 @@ struct _PRWindowClass
   GtkVBoxClass parent_class;
 };
 
-GtkType          prWindow_get_type        (void);
-GtkWidget*     prWindow_new             (void);
+GtkType prWindow_get_type(void);
+GtkWidget* prWindow_new(void);
 
 #ifdef __cplusplus
 }
